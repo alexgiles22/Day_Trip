169428ca87858f0f57159708be468207368cf953
Cameron Giles <alex.stump827@gmail.com>
2022-09-20 23:46:03 -0500

    Finished my functions, now I will work on the dictionary and modifying the values

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ food_list = ['Pasta', 'Steak', 'Sushi']
 
 travel_list = ['Car', 'Train', 'Hoverboard']
 
-fun_list = ['Going shopping', 'Seeing a movie', 'Driving around and looking mansions']
+fun_list = ['Going shopping', 'Seeing a movie', 'Driving around and looking at mansions']
 
 day_trip_dictionary = {
     "Transport": "",
     "Destination": "", 
     "Food": "",
-    "Entertainment": "",
+    "Activity": "",
 }
 
 # travel = random.choice(travel_list)
@@ -33,4 +33,34 @@ def travel_selection():
         user_choice = input(f"How does taking a {travel} sound? (Y/N) ")
     print("That soulds like fun!")
 
+def place_selection():
+    place = random.choice(place_list)
+    user_choice = input(f"Lets' pick a fun place to go! How does {place} sound? (Y/N) ")
+    while user_choice != "Y":
+        print("Ok, let's find somewhere else.")
+        place = random.choice(place_list)
+        user_choice = input(f"How about {place}? (Y/N) ")
+    print("That sounds like a cool place!")
+
+def food_selection():
+    food = random.choice(food_list)
+    user_choice = input(f"Time for something tasty! How about {food}? (Y/N) ")
+    while user_choice != "Y":
+        print("We'll find something else.")
+        food = random.choice(food_list)
+        user_choice = input(f"What about {food} instead? (Y/N) ")
+    print("Sounds tasty!")
+
+def activity_selection():
+    fun = random.choice(fun_list)
+    user_choice = input(f"How about {fun} while you're out? (Y/N) ")
+    while user_choice != "Y":
+        print("We can find something else to do.")
+        fun = random.choice(fun_list)
+        user_choice = input(f"How does {fun} sound? (Y/N) ")
+    print("Thats a fun thing to do!")
+
 travel_selection()
+place_selection()
+food_selection()
+activity_selection()
