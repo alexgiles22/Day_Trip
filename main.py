43b2a389bd56c348021ead87c6750e5e9de51c60
Cameron Giles <alex.stump827@gmail.com>
2022-09-20 19:58:22 -0500

    Created a function to randomy select a mode of transportation. I still need to learn how to update the dictionary, but I will continue creating functions.

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+
+import random
+
+place_list = ['Austin', 'Waco', 'Fort Worth',]
+
+food_list = ['Pasta', 'Steak', 'Sushi']
+
+travel_list = ['Car', 'Train', 'Hoverboard']
+
+fun_list = ['Going shopping', 'Seeing a movie', 'Driving around and looking mansions']
+
+day_trip_dictionary = {
+    "Transport": "",
+    "Destination": "", 
+    "Food": "",
+    "Entertainment": "",
+}
+
+# travel = random.choice(travel_list)
+# food = random.choice(food_list)
+# place = random.choice(place_list)
+# fun = random.choice(fun_list)
 print("")
-print("Hello world!")
+print("Lets plan a day trip!")
 print("")
-print("this is a test commit")
-print("")
+
+def travel_selection():
+    travel = random.choice(travel_list)
+    user_choice = input(f"How does taking a {travel} sound? (Y?N) ")
+    while user_choice != "Y":
+        print("Ok, let's try something else.")
+        travel = random.choice(travel_list)
+        user_choice = input(f"How does taking a {travel} sound? (Y/N) ")
+    print("That soulds like fun!")
+
+travel_selection()
